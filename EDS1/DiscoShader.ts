1d8c9c808f099c8df10c8e34e5354e0c5cfb8dc1
x1n13y84issmd42 <gaddeena@gmail.com>
2015-10-26 05:36:42 +0200

    Better text alignment in DiscoShader

--- EDS1/DiscoShader.ts
+++ EDS1/DiscoShader.ts
@@ -63,11 +63,11 @@ class DiscoShader extends Tsar.Render.Shader
 		var sizeStep = (msize - bsize) / (this.depth / 2);
 		var size = bsize - (sizeStep * (this.depth / 2));
 
-		var pdv = new Tsar.Math.float2(-this.offset.x, -this.offset.y);
+		var pdv = new Tsar.Math.float2(-this.offset.x / 3, -this.offset.y / 3);
 		var pdvs = pdv.ndiv(this.depth);
 		
 		C.font = "bold " + size + "px Monoton";
-		var textW = C.measureText(this.text).width / 2;
+		var textW = 0;//C.measureText(this.text).width / 2;
 		var pt = (new Tsar.Math.float2(this.pt.x - textW, this.pt.y)).sub(pdvs.nmul(this.depth/2));
 
 		var c = new Tsar.Math.Color(64, 128, 255, 1);
@@ -88,14 +88,15 @@ class DiscoShader extends Tsar.Render.Shader
 				C.fillStyle = c.rgba();
 				C.font = "bold " + size * 2 + "px Monoton";
 				var hitW = C.measureText(this.text).width / 2;
-				C.context.fillText(this.text, pt.x - (hitW-textW)/2, pt.y);
+				C.context.fillText(this.text, pt.x - (hitW)/2, pt.y);
 				c.a = 1;
 			}
 			else
 			{
 				C.fillStyle = c.rgba();
 				C.font = "bold " + size + "px Monoton";
-				C.context.fillText(this.text, pt.x, pt.y);
+				var tW = C.measureText(this.text).width / 2;
+				C.context.fillText(this.text, pt.x - (tW), pt.y);
 			}
 
 			pt = pt.add(pdvs);
